7535e0820e205975c1b775e0b610f06b82bec71d
Ibrahim Rahman <ibrahimrahmancse@gmail.com>
2024-09-04 10:15:52 +0530

    New coad update

---
+++ Basic.cpp/airthmeticOperator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    int x = 5 ;
+    cout<<x<<endl ;
+    int y = 2 ;
+    cout<<y<<endl ;
+
+    cout<< x+y <<endl ;
+    cout<< x-y <<endl ;
+    cout<< x*y <<endl ;
+    cout<< x/y <<endl ;
+
+    float z = x/y ;
+    cout<< z <<endl ;
+
+    cout<<"\n we are not getting decimal answer because , both the No , x amd y are  int data type \n " ;
+}
